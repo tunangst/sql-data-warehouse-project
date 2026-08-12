f51c16a057e26549f13c1bddf5d2e7c2f30e522b
TRU <trunangst@gmail.com>
2026-08-12 14:14:09 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -23,11 +23,14 @@ IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
 	prd_key NVARCHAR(50),
+	cc_cat_id NVARCHAR(10),
+	cc_prd_key NVARCHAR(20),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
-	prd_line NVARCHAR(5),
+	prd_line NVARCHAR(20),
 	prd_start_dt DATE,
 	prd_end_dt DATE,
+	cc_prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -37,9 +40,9 @@ CREATE TABLE silver.crm_sales_details(
 	sls_ord_num NVARCHAR(10),
 	sls_prd_key NVARCHAR(10),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_sales_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
